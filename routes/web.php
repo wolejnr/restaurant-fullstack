e22231f46bb3e19de531f8f97ce34df739b5f77d
Adewole Adewumi <wolejr@gmail.com>
2020-03-04 02:26:01 -0500

    Created create and edit forms for food categories

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,14 @@ Route::get('/admin/food-categories', function () {
     return view('admin/food-categories/all');
 });
 
+Route::get('/admin/food-categories/create', function () {
+    return view('admin/food-categories/create');
+});
+
+Route::get('/admin/food-categories/{id}/edit', function () {
+    return view('admin/food-categories/edit');
+});
+
 Route::get('/admin/register', function () {
     return view('admin/register');
 });
