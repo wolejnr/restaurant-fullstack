aee3b5f131f572f8975f680597726d18efe797d9
Adewole Adewumi <wolejr@gmail.com>
2020-03-05 20:16:52 -0500

    Created the users pages

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,11 @@ Route::get('/admin/food-items', 'admin\FoodItemsController@index');
 Route::get('/admin/food-items/create', 'admin\FoodItemsController@create');
 Route::get('/admin/food-items/{id}/edit', 'admin\FoodItemsController@edit');
 
+# Admin Users
+Route::get('/admin/users', 'admin\UsersController@index');
+Route::get('/admin/users/create', 'admin\UsersController@create');
+Route::get('/admin/users/{id}/edit', 'admin\UsersController@edit');
+
 # Admin Customers
 Route::get('/admin/offers-members', 'admin\CustomersController@allOffersMembers');
 Route::get('/admin/reservations', 'admin\CustomersController@allReservations');
