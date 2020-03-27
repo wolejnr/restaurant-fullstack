e04a9a4857c80d3840bcd0eb9ed8370cd4b9cbf3
Adewole Adewumi <wolejr@gmail.com>
2020-03-26 21:50:10 -0400

    showed menu options depending on roles

--- resources/views/includes/admin-side-menu.blade.php
+++ resources/views/includes/admin-side-menu.blade.php
@@ -21,6 +21,8 @@
                                     </ul>
                                 </div>
                             </li>
+
+                            @if(Auth::user()->isAdmin())
                             <li class="nav-item">
                                 <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-rocket"></i>Menu</a>
                                 <div id="submenu-2" class="collapse submenu" style="">
@@ -56,6 +58,8 @@
                                     </ul>
                                 </div>
                             </li>
+                            @endif
+
                             <li class="nav-item">
                                 <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-3" aria-controls="submenu-3"><i class="fa fa-fw fa-rocket"></i>Customers</a>
                                 <div id="submenu-3" class="collapse submenu" style="">
@@ -87,6 +91,8 @@
                                     </ul>
                                 </div>
                             </li>
+
+                            @if(Auth::user()->isAdmin())
                             <li class="nav-item">
                                 <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-4" aria-controls="submenu-4"><i class="fa fa-fw fa-rocket"></i>Users</a>
                                 <div id="submenu-4" class="collapse submenu" style="">
@@ -100,22 +106,27 @@
                                     </ul>
                                 </div>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-5" aria-controls="submenu-5"><i class="fa fa-fw fa-rocket"></i>Settings</a>
-                                <div id="submenu-5" class="collapse submenu" style="">
-                                    <ul class="nav flex-column">
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="/admin/settings/general">General Settings<span class="badge badge-secondary">New</span></a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="/admin/settings/seo">SEO<span class="badge badge-secondary">New</span></a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="/admin/settings/social">Social Accounts<span class="badge badge-secondary">New</span></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </li>
+                            @endif
+
+                            @if(Auth::user()->isAdmin())
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-5" aria-controls="submenu-5"><i class="fa fa-fw fa-rocket"></i>Settings</a>
+                                    <div id="submenu-5" class="collapse submenu" style="">
+                                        <ul class="nav flex-column">
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="/admin/settings/general">General Settings<span class="badge badge-secondary">New</span></a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="/admin/settings/seo">SEO<span class="badge badge-secondary">New</span></a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="/admin/settings/social">Social Accounts<span class="badge badge-secondary">New</span></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </li>
+                            @endif
+                            
                         </ul>
                     </div>
                 </nav>
